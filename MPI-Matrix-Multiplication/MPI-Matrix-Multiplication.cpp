b18494054ecabe7d74eeb82b47c1f7d08d255aad
Alexey-Sagaydak <99820328+Alexey-Sagaydak@users.noreply.github.com>
2023-12-05 14:26:02 +0700

    fix

---
+++ MPI-Matrix-Multiplication/MPI-Matrix-Multiplication.cpp
@@ -0,0 +1,94 @@
+﻿#include <mpi.h>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+void fillMatrixAndVector(std::vector<std::vector<int>>& matrix, std::vector<int>& vector) {
+    // Заполнение матрицы и вектора единицами (в данном случае, случайными значениями)
+    // Замените этот код вашей логикой по заполнению
+    srand(time(nullptr));
+    for (auto& row : matrix) {
+        for (int& element : row) {
+            element = rand() % 10;  // Произвольный диапазон для примера
+        }
+    }
+
+    for (int& element : vector) {
+        element = rand() % 10;  // Произвольный диапазон для примера
+    }
+}
+
+void printMatrix(const std::vector<std::vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int element : row) {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void printVector(const std::vector<int>& vector) {
+    for (int element : vector) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    const int matrixSize = 4;  // Замените это значение на ваш размер матрицы
+
+    // Процесс с рангом 0 заполняет матрицу и вектор
+    std::vector<std::vector<int>> matrix;
+    std::vector<int> vector(matrixSize);
+
+    if (rank == 0) {
+        matrix.resize(matrixSize, std::vector<int>(matrixSize));
+        fillMatrixAndVector(matrix, vector);
+    }
+
+    // Рассылка матрицы и вектора с использованием коллективных взаимодействий
+    MPI_Bcast(vector.data(), matrixSize, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        // Рассылка строк матрицы
+        for (int i = 0; i < matrixSize; ++i) {
+            MPI_Bcast(matrix[i].data(), matrixSize, MPI_INT, 0, MPI_COMM_WORLD);
+        }
+    }
+
+    // Каждый процесс выполняет умножение своих строк на вектор
+    std::vector<int> localResult(matrixSize, 0);
+    for (int i = 0; i < matrixSize; ++i) {
+        for (int j = 0; j < matrixSize; ++j) {
+            localResult[i] += matrix[i][j] * vector[j];
+        }
+    }
+
+    // Сбор результатов с использованием декартовой топологии и коммуникатора строки решетки
+    std::vector<int> globalResult(matrixSize, 0);
+    MPI_Reduce(localResult.data(), globalResult.data(), matrixSize, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    // Вывод результатов на экран (может быть отключено для больших данных)
+    if (rank == 0) {
+        std::cout << "Matrix:" << std::endl;
+        printMatrix(matrix);
+
+        std::cout << "Vector:" << std::endl;
+        printVector(vector);
+
+        std::cout << "Result:" << std::endl;
+        printVector(globalResult);
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
--- PP-MPI/main.cpp
+++ PP-MPI/main.cpp
@@ -3,6 +3,12 @@
 #include <mpi.h>
 
 /*
+Что не так:
+Нужно чтобы нулевой процесс отправлял границы каждому процессу с помощью send,а процессы собирали
+бы их с помощью receive. В конце нулевой процесс собирал бы результаты с помощью reduce
+*/
+
+/*
 ИНСТРУКЦИЯ ПО ЗАПУСКУ:
 1) Открыть командную строку в директории с исполняемым файлом.
 2) Выполнить команду mpiexec -np X ./MPI-Integration.exe
@@ -13,50 +19,54 @@ double func(double x) {
     return log(x);
 }
 
-// Параллельный метод средних прямоугольников
-double parallelRectangleIntegration(double a, double b, int n, int rank, int size) {
-    double h = (b - a) / n;  // Шаг интегрирования
+double integrateSubInterval(double a, double b, int n) {
+    double h = (b - a) / n;
     double localSum = 0.0;
 
-    // Локальное вычисление частичной суммы
+    for (int i = 1; i <= n; ++i) {
+        double x = a + (i - 0.5) * h;
+        localSum += func(x);
+    }
+
+    localSum *= h;
+
+    return localSum;
+}
+
+double parallelRectangleIntegration(double a, double b, int n, int rank, int size) {
+    double h = (b - a) / n;
+    double localSum = 0.0;
+
     for (int i = rank + 1; i <= n; i += size) {
         double x = a + (i - 0.5) * h;
         localSum += func(x);
     }
 
-    localSum *= h;  // Умножаем на шаг интегрирования
+    localSum *= h;
 
-    // Обмен границами и сборка результатов
-    if (rank == 0) {
-        double totalSum = localSum;
-
-        // Отправка локальной суммы другим процессам
-        for (int i = 1; i < size; ++i) {
-            MPI_Recv(&localSum, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            totalSum += localSum;
+    // Отправка границ другим процессам
+    for (int i = 0; i < size; ++i) {
+        if (i != rank) {
+            MPI_Send(&a, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&b, 1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
         }
+    }
 
-        return totalSum;
+    // Отправка локальной суммы нулевому процессу
+    if (rank != 0) {
+        MPI_Send(&localSum, 1, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
     }
-    else {
-        // Отправка локальной суммы нулевому процессу
-        MPI_Send(&localSum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-        return 0.0;  // Процессы, отличные от нулевого, возвращают фиктивное значение
-    }
+
+    return localSum;  // Процессы, отличные от нулевого, возвращают локальную сумму
 }
 
 int main() {
     MPI_Init(NULL, NULL);
 
-    // Number of processes
-    int size;
+    int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    // Number of current process
-    int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    // Processor name
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_len;
     MPI_Get_processor_name(processor_name, &name_len);
@@ -66,23 +76,39 @@ int main() {
         ***********************\n",
         rank, processor_name, size);
 
-    double a = 1.0;  // Начальная граница интервала
-    double b = 10000.0;  // Конечная граница интервала
-    int n = 100000000; // Количество прямоугольников
-    
+    double a = 1, b = 500;
+    int n = 100000000;
+    double localSum = 0.0;
+    double totalSum = 0.0;
+
     double startTime = MPI_Wtime();
 
-    // Вызов параллельного метода средних прямоугольников
-    double result = parallelRectangleIntegration(a, b, n, rank, size);
+    // Нулевой процесс получает локальную сумму и границы от других процессов
+    if (rank == 0) {
+        for (int i = 1; i < size; ++i) {
+            MPI_Recv(&localSum, 1, MPI_DOUBLE, i, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            totalSum += localSum;
+        }
+    }
+    else {
+        // Процессы, отличные от нулевого, получают границы и интегрируют свой подинтервал
+        MPI_Recv(&a, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&b, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        localSum = parallelRectangleIntegration(a, b, n, rank, size);
+    }
+
+    // Использование MPI_Reduce для получения окончательного результата
+    MPI_Reduce(&localSum, &totalSum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
     double endTime = MPI_Wtime();
 
     if (rank == 0) {
-        std::cout << "Integral value: " << result << std::endl;
-        std::cout << "Total time: " << (endTime - startTime) * 1000 << " мс" << std::endl;
+        std::cout << "Integral value: " << totalSum << std::endl;
+        std::cout << "Total time: " << (endTime - startTime) * 1000 << " ms" << std::endl;
     }
 
     MPI_Finalize();
 
     return 0;
 }
+
